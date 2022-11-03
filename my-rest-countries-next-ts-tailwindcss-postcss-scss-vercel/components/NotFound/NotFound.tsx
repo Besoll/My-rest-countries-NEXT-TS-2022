834b09f9d52c80444354c433b3769ab0d9f94e8a
Besoll <b.kavzharadze@gmail.com>
2022-11-03 15:11:18 +0200

    Commit

--- my-rest-countries-next-ts-tailwindcss-postcss-scss-vercel/components/NotFound/NotFound.tsx
+++ my-rest-countries-next-ts-tailwindcss-postcss-scss-vercel/components/NotFound/NotFound.tsx
@@ -1,36 +1,24 @@
-import { Back, NotFoundIcon } from "@assets/icons"
-import { Button } from "@components/index"
-import s from "./NotFound.module.css"
-import { useRouter } from 'next/router'
-
-
+import { Back } from "@assets/icons";
+import { Button } from "@components/index";
+import s from "./NotFound.module.css";
+import { useRouter } from "next/router";
 
 const NotFound = ({ returnHome }: { returnHome?: boolean }) => {
-
-  const router = useRouter()
+  const router = useRouter();
 
   return (
     <div className={s.root}>
-        <div className="svgContainer">
-        <Button 
-          type={"primary"}
-          icon={<NotFoundIcon />}
-          text={" "}
-          onClick={() => router.back()}
-         />
-        </div>
-        <h1 className={s.h1}>404 error</h1>
-        <p className={s.p}>Uh oh! Looks like you got lost.</p>
-        <p className={s.p}>Go back to the homepage</p>
-        <Button 
-          type={"primary"}
-          icon={<Back />}
-          text={" Go HOME!"}
-          onClick={() => router.back()}
-         />
+      <h1 className={s.h1}>404 error</h1>
+      <p className={s.p}>Uh oh! Looks like you got lost.</p>
+      <p className={s.p}>Go back to the homepage</p>
+      <Button
+        type={"primary"}
+        icon={<Back />}
+        text={" Go HOME!"}
+        onClick={() => router.back()}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
-
+export default NotFound;
